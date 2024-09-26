5b1eca5bb7c1c7ea37322f6e005f520f562b71b8
DannyB25 <dabrion25@gmail.com>
2024-09-25 23:08:31 -0700

    committing more to main

---
+++ Boolean Logic Submission Assignment/Boolean Logic Submission Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BooleanLogicSubmissionAssignment
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?"); //prints text 
+            string userAge = Console.ReadLine(); //transforms user response into variable userAge
+            int ageOfUser = Convert.ToInt32(userAge);
+
+            Console.WriteLine("Have you ever had a DUI? (answer: true or false)"); //prints text
+            string userDui = Console.ReadLine(); //transforms user response into variable userDui
+            bool duiBool = Convert.ToBoolean(userDui);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string userTickets = Console.ReadLine(); //transforms user response into variable userTickets
+            int ticketNumb = Convert.ToInt32(userTickets);
+
+            bool qualification = ageOfUser > 15 && duiBool != true && ticketNumb <= 3;
+            Console.WriteLine(qualification);
+            Console.ReadLine();
+
+
+        }
+
+    } 
+
+} 
---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine(true && false); //asks if both are true. prints false
+                Console.WriteLine(true && true); //asks if both are true, prints true
+                Console.WriteLine(false && false); //asks if both are true, prints false 
+
+                Console.WriteLine(true || true); //asks if one is true, prints true
+                Console.WriteLine(true || false); //asks if one is true, prints true 
+                Console.WriteLine(false || true); //asks if one is true, prints false 
+
+                Console.WriteLine(true == true); //asks if true equals true, prints true
+                Console.WriteLine(true == false); //asks if true equals false, prints false
+                Console.WriteLine(false == false); //asks if false equals false, prints true
+
+                Console.WriteLine(true != true); //asks if true does not equal true, prints false
+                Console.WriteLine(true != false); //asks if true does not equal false, prints true
+                Console.WriteLine(false != false); //asks if false does not equal false, print false
+
+                Console.WriteLine(true ^ true); //asks if only one is true but not both, prints false
+                Console.WriteLine(true ^ false); //asks if only one is true but not both, prints true
+                Console.WriteLine(false ^ false); //asks if only one is true but not both, prints false
+
+                Console.WriteLine(true && true && true && false); //asks if all are true, prints false
+                Console.WriteLine(true && true && true || false); //asks if all are true or if last expression is false, prints true
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
