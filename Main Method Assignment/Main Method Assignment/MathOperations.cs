ca9471bf7ba4e4b6fab8abe2fcdac58e9ea094ba
DannyB25 <dabrion25@gmail.com>
2024-10-02 00:15:51 -0700

    Committing Main Method Assignment to Main

---
+++ Main Method Assignment/Main Method Assignment/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class MathOperations
+{
+    //Method that takes in an integer, adds 100, and returns the result 
+    public int Calculate (int number)
+    {
+        return number + 100; //addition operation 
+    }
+
+    //Overloaded method that takes a decimal, subtracts 5, and returns the result as an integer
+    public int Calculate(decimal number)
+    {
+        return (int)(number - 20); //subtraction operation 
+    }
+
+    //another overloaded method that takes a string, converts it to an integer, and multiplies it by 2 
+    public int Calculate(string numberString)
+    {
+        if (int.TryParse(numberString, out int number))
+        {
+            return number * 10; //multiplication operation
+        }
+        else
+        {
+            throw new FormatException("Error: Invalid input!");
+        }
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //create an instance of the MathOperations class
+        MathOperations mathOps = new MathOperations();
+
+        //call the method with an integer
+        Console.WriteLine("Enter an integer and then I'll add 100:");
+        if (int.TryParse(Console.ReadLine(), out int intInput))
+        {
+            int intResult = mathOps.Calculate(intInput);
+            Console.WriteLine($"Result after adding 100: {intResult}");
+        }
+        //call the method with a decimal 
+        Console.WriteLine("Enter a decimal and I will subtract 20:");
+        if (decimal.TryParse(Console.ReadLine(), out decimal decimalInput))
+        {
+            int decimalResult = mathOps.Calculate(decimalInput);
+            Console.WriteLine($"Result after subtracting 20 (converted to int): {decimalResult}");
+        }
+
+        //call method with a string
+        Console.WriteLine("Enter a string that equates to an integer to multiply by 10:");
+        string stringInput = Console.ReadLine();
+        try
+        {
+            int stringResult = mathOps.Calculate(stringInput);
+            Console.WriteLine($"Result after multiplying by 10: {stringResult}");
+        }
+        catch (FormatException ex)
+        { 
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
