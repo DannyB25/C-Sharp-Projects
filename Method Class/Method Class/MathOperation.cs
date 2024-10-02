97c6f6d2d0b29e26558cd0f7655625425d729c99
DannyB25 <dabrion25@gmail.com>
2024-10-02 12:12:03 -0700

    committing math operation to main

---
+++ Method Class/Method Class/MathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public class MathOperation
+{
+    //create method that is fed two integers
+    public void DisplayMathOperation(int firstNumber, int secondNumber)
+    {
+        //perform a math operation on the first integer 
+        int result = firstNumber + 30;
+
+        //display math operation w/ first number and then just the second number
+        Console.WriteLine($"After adding 30 to {firstNumber} the outcome is: {result}");
+        Console.WriteLine($"The second number inputted is: {secondNumber}");
+
+    }
+}
---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //creating an instance of the class
+        MathOperation mathOp = new MathOperation();
+
+        //request user input and record their answers in a variable, while converting to an integer
+        Console.WriteLine("Enter the first number:");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        //request user input and record their answers in a variable, while converting to an integer
+        Console.WriteLine("Enter a second number:");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        //call the method and pass in two user numbers (the standard way)
+        mathOp.DisplayMathOperation(firstNumber, secondNumber);
+
+        //calling the method by using named parameters
+        mathOp.DisplayMathOperation(secondNumber: secondNumber, firstNumber: firstNumber);
+        
+        //keeps text on the console 
+        Console.ReadLine();
+    }
+}
