ad384ba437ed3c836efc14730a8bb7c308e2c524
DannyB25 <dabrion25@gmail.com>
2024-10-10 12:06:31 -0700

    Commit to main

---
+++ MVC Intro/MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MVC_Intro.Models;
+using System.Diagnostics;
+
+namespace MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Jesse";
+            user.LastName = "Johnson";
+            user.Age = 32;
+            return View(user);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
